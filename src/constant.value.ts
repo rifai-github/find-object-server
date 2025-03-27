54b322eb9442376b6f0b0e5c098173b578161c5b
Rifai Kridolaksono <rivayx@gmail.com>
2025-03-27 07:23:39 +0700

    change to laravel 😓

--- src/constant.value.ts
+++
@@ -1,4 +0,0 @@
-export class SQLTable {
-    static Users = 'users';
-    static Games = 'games';
-}
--- src/controller/utils.controller.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express';
-import { ResponseFactory } from '../helpers/utils';
-
-export async function healthCheck(req: Request, res: Response) {
-    res.send(ResponseFactory.success('API OK!'));
-}
